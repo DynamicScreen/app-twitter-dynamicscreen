ca6f7dbbdbd41edac7fecb6d70d217685a8ec388
Corentin Martin <corentin.martin@dynamicscreen.com>
2021-11-17 17:31:59 +0100

    test

--- src/Twitter/TwitterSlideHandler.php
+++ src/Twitter/TwitterSlideHandler.php
@@ -15,7 +15,58 @@ class TwitterSlideHandler extends SlideHandler
 
     public function fetch(ISlide $slide): void
     {
-        $default = $this->getDefaultOptions();
+        $options = $slide->getOptions();
+
+        $driver = $this->getAuthProvider($slide->getAccounts());
+        if ($driver == null) return;
+
+        $expiration = Carbon::now()->addHour();
+        $cache_key = "{$this->getProviderIdentifier()}::{$options['username']}_{$options['page']}";
+        $api_response = app('cache')->remember($cache_key, $expiration, function () use ($options, $driver, $account) {
+            if (starts_with($options['username'], '#')) {
+                $response = $driver->search('#' . ltrim($options['username'], '#'),
+                    ['count' => $options['page'] * 4]);
+                if (!isset($response->statuses)) {
+                    return [];
+                }
+                $collection_tweets = collect($response->statuses)->map(function ($tweet){
+                    return $this->presentTweet($tweet);
+                });
+                return $collection_tweets->toJson();
+            }
+            else{
+                $collection_tweets = collect($driver->getTweetsOf('@' . ltrim($options['username'], '@'), ['count' => $options['page'] * 4]));
+                foreach($collection_tweets as $tweet) {
+                    $collection_tweets_formatted[] = $this->presentTweet($tweet);
+                }
+                return json_encode($collection_tweets_formatted);
+            }
+        });
+
+        foreach (collect(json_decode($api_response))->chunk(4) as $chunk){
+            $tweets = [];
+            $tweetImg =false;
+            foreach ($chunk as $tweet){
+                if($tweet->media_url and $tweetImg == false){
+                    $tweetImg = $tweet;
+                }
+                else{
+                    $tweets[] = $tweet;
+                }
+            }
+            $lastTweet = null;
+
+            if($tweetImg == false){
+                $lastTweet = collect($tweets)->first();
+                $tweets = collect($tweets)->slice(1);
+            }
+            $this->addSlide([
+                'tweets' => $tweets,
+                'title' => $options['title'],
+                'firstImg' => $tweetImg,
+                'lastTweet' => $lastTweet
+            ]);
+        }
 
         $this->addSlide([]);
     }
---
+++ src/TwitterDriver/TwitterAuthProviderHandler.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace DynamicScreen\Twitter\TwitterDriver;
+
+use Abraham\TwitterOAuth\TwitterOAuthException;
+use DynamicScreen\SdkPhp\Handlers\OAuthProviderHandler;
+use Illuminate\View\View;
+use Illuminate\Support\Str;
+use Abraham\TwitterOAuth\TwitterOAuth;
+use Illuminate\Support\Facades\Session;
+
+class TwitterAuthProviderHandler extends OAuthProviderHandler
+{
+    public static string $provider = 'twitter';
+
+    public $default_config = [];
+
+    public function __construct($config = null)
+    {
+        $this->default_config = $config;
+    }
+
+    public function identifier()
+    {
+        return 'twitter-driver';
+    }
+
+    public function name()
+    {
+        return "Twitter";
+    }
+
+    public function description()
+    {
+        return "OAuth Twitter";
+    }
+
+    public function icon()
+    {
+        return "fab fa-twitter";
+    }
+
+    public function color()
+    {
+        return '#00acee';
+    }
+
+//    public function renderOptions(Account $account) : View
+//    {
+//        try {
+//            $infos = $this->getUserInfos($account);
+//            return view('accounts-options.twitter', compact('account', 'infos'));
+//        } catch (\Exception $e) {
+//            return view('accounts-options.twitter', compact('account'));
+//        }
+//    }
+
+    public function testConnection($config = null)
+    {
+        $options = $config ?? $this->default_config;
+
+        $twitterConnection = new TwitterOAuth(config('ttwitter.CONSUMER_KEY'), config('ttwitter.CONSUMER_SECRET'), $options['oauth_token'], $options['oauth_token_secret']);
+        $twitterConnection->get('application/rate_limit_status');
+        return response('', $twitterConnection->getLastHttpCode());
+
+    }
+
+    public function getUserInfos($config = null)
+    {
+        $options = $config ?? $this->default_config;
+
+        $twitterConnection = new TwitterOAuth(config('ttwitter.CONSUMER_KEY'), config('ttwitter.CONSUMER_SECRET'), $options['oauth_token'], $options['oauth_token_secret']);
+        $infos = $twitterConnection->get('users/show', ['user_id' => $options['user_id']]);
+        if ($twitterConnection->getLastHttpCode() != 200) {
+            throw new \Exception('Cannot get user informations');
+        }
+        return $infos;
+
+    }
+
+    public function signin($callbackUrl = null)
+    {
+        $callbackUrl = $callbackUrl ?? route('api.oauth.callback');
+
+        $consumer_key = config("services.{$this->getProviderIdentifier()}.client_id");
+        $consumer_secret = config("services.{$this->getProviderIdentifier()}.client_secret");
+        $ds_uuid = 'oauth.twitter.' . (string)Str::uuid();
+        $twitteroauth = new TwitterOAuth($consumer_key, $consumer_secret);
+        $request_token = $twitteroauth->oauth('oauth/request_token', [
+            'oauth_callback' => $callbackUrl,
+        ]);
+
+        if ($twitteroauth->getLastHttpCode() != 200) {
+            throw new \Exception('Request token not found.');
+        }
+
+        Session::put($ds_uuid, ['oauth_token' => $request_token['oauth_token'], 'oauth_token_secret' => $request_token['oauth_token_secret']]);
+
+        $url = $twitteroauth->url(
+            'oauth/authorize', [
+                'oauth_token' => $request_token['oauth_token'],
+            ]
+        );
+
+        return $url;
+    }
+
+    public function callback($request, $redirectUrl)
+    {
+        $consumer_key = config("services.{$this->getProviderIdentifier()}.client_id");
+        $consumer_secret = config("services.{$this->getProviderIdentifier()}.client_secret");
+
+        $oauth_verifier = $request->input('oauth_verifier');
+        $ds_uuid = $request->input('ds_uuid');
+        if (empty($oauth_verifier) || !Session::has($ds_uuid)) {
+            throw new \Exception('Missing token');
+        }
+
+        $connection = new TwitterOAuth($consumer_key, $consumer_secret, Session::get($ds_uuid)['oauth_token'], Session::get($ds_uuid)['oauth_token_secret']);
+        $request->session()->forget($ds_uuid);
+
+        $token = $connection->oauth('oauth/access_token', ['oauth_verifier' => $oauth_verifier]);
+        $token['active'] = true;
+
+        $data = $this->processOptions($token);
+        $dataStr = json_encode($data);
+
+        return redirect()->away($redirectUrl ."&data=$dataStr");
+
+
+//        return route('manager.settings.accounts.edit', ['_spacename' => $space_name, 'account' => $account]);
+    }
+
+    public function createConnection($config = null)
+    {
+        $accountOptions = $config ?? $this->default_config;
+
+        $consumer_key = config("services.{$this->getProviderIdentifier()}.client_id");
+        $consumer_secret = config("services.{$this->getProviderIdentifier()}.client_secret");
+
+        return new TwitterOAuth($consumer_key, $consumer_secret, $accountOptions['oauth_token'], $accountOptions['oauth_token_secret']);
+    }
+
+    public function getTweetsOf($username, $options = [], $config = null)
+    {
+        $config = $config ?? $this->default_config;
+
+        return $this->createConnection($config)
+            ->get('statuses/user_timeline', array_merge([
+                'screen_name' => $username,
+            ], $options));
+    }
+
+    public function search($q, $options = [], $config = null)
+    {
+        $config = $config ?? $this->default_config;
+
+        return $this->createConnection($config)
+            ->get('search/tweets', array_merge([
+                'q' => $q,
+            ], $options));
+    }
+}
