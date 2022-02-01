dbe5e91e896b4ac882f4d037db359727099f983e
Corentin Martin <corentin.martin@dynamicscreen.com>
2022-02-01 18:42:57 +0100

    color logo + wip asset download

--- src/Twitter/Twitter.ts
+++ src/Twitter/Twitter.ts
@@ -2,7 +2,7 @@ import {
   ISlideContext,
   IPublicSlide,
   SlideModule,
-  VueInstance
+  VueInstance, IAssetsStorageAbility, IAssetDownload
 } from "dynamicscreen-sdk-js";
 
 import Tweet from "../Components/Tweet";
@@ -10,71 +10,93 @@ import TweetAttachments from "../Components/TweetAttachments";
 
 export default class TwitterSlideModule extends SlideModule {
     async onReady() {
-        return true;
+      await this.context.assetsStorage().then(async (ability: IAssetsStorageAbility) => {
+        // await ability.downloadAndGet(this.context.slide.data.logo);
+        await ability.downloadAndGet(this.context.slide.data.avatar);
+
+        if (!!this.context.slide.data.media_url) {
+          await ability.downloadAndGet(this.context.slide.data.media_url);
+        }
+      });
+
+      return true
     };
 
     setup(props: Record<string, any>, vue: VueInstance, context: ISlideContext) {
-        const { h, reactive, computed, ref } = vue;
-        const slide = reactive(this.context.slide) as IPublicSlide
+      const { h, reactive, computed, ref } = vue;
+      const slide = reactive(this.context.slide) as IPublicSlide
 
-        // const logo = ref(slide.data.logo);
+      // const logo = ref(slide.data.logo);
+      const text = ref(slide.data.text);
+      const userPicture = ref(slide.data.avatar);
+      const userName = ref(slide.data.name);
+      const publicationDate = ref(slide.data.created_at);
 
-        // const isTweetWithAttachment = computed(() => {
-        //     return !!slide.data.attachmentUrl;
-        // });
-      console.log(slide.data);
-        const isTweetWithAttachment = computed(() => {
-          return !!slide.data.media_url;
-        })
-
+      // const isTweetWithAttachment = computed(() => {
+      //     return !!slide.data.attachmentUrl;
+      // });
       // const tweetAttachment = isTweetWithAttachment.value ? ref(slide.data.attachmentUrl) : null;
-      const tweetAttachment = isTweetWithAttachment.value ? ref(slide.data.media_url) : null;
-        const text = ref(slide.data.text);
-        const userPicture = ref(slide.data.avatar);
-        const userName = ref(slide.data.name);
-        const publicationDate = ref(slide.data.created_at);
 
-        this.context.onPlay(async () => {
-          this.context.anime({
-            targets: "#tweet",
-            translateX: [-40, 0],
-            opacity: [0, 1],
-            duration: 600,
-            easing: 'easeOutQuad'
-          });
-          this.context.anime({
-            targets: "#user",
-            translateX: [-40, 0],
-            opacity: [0, 1],
-            duration: 600,
-            delay: 250,
-            easing: 'easeOutQuad'
-          });
+      const isTweetWithAttachment = computed(() => {
+        return !!slide.data.media_url;
+      })
+      const tweetAttachment = isTweetWithAttachment.value ? ref(slide.data.media_url) : null;
+
+      this.context.onPrepare(async () => {
+        isTweetWithAttachment.value && await this.context.assetsStorage().then(async (ability: IAssetsStorageAbility) => {
+          tweetAttachment.value = await ability.getDisplayableAsset(slide.data.media_url).then((asset) => asset.displayableUrl());
         });
 
-        return () => h("div", {
-            class: "w-full h-full flex justify-center items-center"
-          }, [
-            //@ts-ignore
-            !isTweetWithAttachment.value && h(Tweet, {
-                text: text.value,
-                userPicture: userPicture.value,
-                userName: userName.value,
-                publicationDate: publicationDate.value,
-                class: "w-1/2"
-              }),
-            //@ts-ignore
-            isTweetWithAttachment.value && h(TweetAttachments, {
-              text: text.value,
-              userPicture: userPicture.value,
-              userName: userName.value,
-              publicationDate: publicationDate.value,
-              tweetAttachment: tweetAttachment.value,
-              class: "w-full h-full"
-            }),
-            // h("i", {
-            //   class: "w-16 h-16 absolute top-10 right-10 portrait:bottom-10 portrait:top-auto text-blue-400 " + logo
-            // })
-          ])
+        await this.context.assetsStorage().then(async (ability: IAssetsStorageAbility) => {
+          userPicture.value = await ability.getDisplayableAsset(slide.data.avatar).then((asset) => asset.displayableUrl());
+        });
+
+        // await this.context.assetsStorage().then(async (ability: IAssetsStorageAbility) => {
+        //   logo.value = await ability.getDisplayableAsset(logo.value).then((asset) => asset.displayableUrl());
+        // });
+      });
+
+      this.context.onPlay(async () => {
+        this.context.anime({
+          targets: "#tweet",
+          translateX: [-40, 0],
+          opacity: [0, 1],
+          duration: 600,
+          easing: 'easeOutQuad'
+        });
+        this.context.anime({
+          targets: "#user",
+          translateX: [-40, 0],
+          opacity: [0, 1],
+          duration: 600,
+          delay: 250,
+          easing: 'easeOutQuad'
+        });
+      });
+
+      return () => h("div", {
+        class: "w-full h-full flex justify-center items-center"
+      }, [
+        //@ts-ignore
+        !isTweetWithAttachment.value && h(Tweet, {
+          text: text.value,
+          userPicture: userPicture.value,
+          userName: userName.value,
+          publicationDate: publicationDate.value,
+          class: "w-1/2"
+          }),
+        //@ts-ignore
+        isTweetWithAttachment.value && h(TweetAttachments, {
+          text: text.value,
+          userPicture: userPicture.value,
+          userName: userName.value,
+          publicationDate: publicationDate.value,
+          tweetAttachment: tweetAttachment.value,
+          class: "w-full h-full"
+        }),
+        // h("i", {
+        //   class: "w-16 h-16 absolute top-10 right-10 portrait:bottom-10 portrait:top-auto text-blue-400 " + logo
+        // })
+      ])
     }
 }
